3f5c3cb7348d561ae008f80727ef7b103b1af968
blackhat <jeromeabel20@gmail.com>
2024-01-03 00:06:16 +0530

    aaa

--- resources/views/user/aifiles/allfiles.blade.php
+++ resources/views/user/aifiles/allfiles.blade.php
@@ -41,7 +41,7 @@
                                     <td>{{ $file->ai_details }}</td>
                                     <td>{{ $file->ai_type }}</td>
                                     <td>
-									<a href="{{ $file->s3_path }}" target="_blank"><button>View Url</button></a>
+									<a href="{{ $file->s3_path }}" target="_blank"><button class="btn btn-primary">View Url</button></a>
 		
 									
 								
